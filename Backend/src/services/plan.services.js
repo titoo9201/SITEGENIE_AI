ee8288926b9f23d12eab6d462f0577d97811c793
Titoo Singh <148178131+titoo9201@users.noreply.github.com>
2026-03-03 02:14:10 +0530

    Refactor plan names to lowercase
    Updated plan names to lowercase for consistency.

--- Backend/src/services/plan.services.js
+++ Backend/src/services/plan.services.js
@@ -1,19 +1,21 @@
- const PLANS = {
-  free: {
-    price: 0,
-    credits: 200,
-    plan: "free",
-  },
-  student: {
-    price: 199,
-    credits: 400,
-    plan: "Student pro",
-  },
-  pro: {
-    price: 399,
-    credits: 600,
-    plan: "Creator pro",
-  },
+const PLANS = {
+ free: {
+  price: 0,
+  credits: 200,
+  plan: "free"
+ },
+
+ student: {
+  price: 199,
+  credits: 400,
+  plan: "student"
+ },
+
+ pro: {
+  price: 399,
+  credits: 600,
+  plan: "pro"
+ }
 };
 
 
