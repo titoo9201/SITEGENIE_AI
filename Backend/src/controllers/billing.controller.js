756db93e07256f6a020e62b55845c265ea1e706e
Titoo Singh <148178131+titoo9201@users.noreply.github.com>
2026-03-03 02:41:34 +0530

    Refactor billing function for better error handling

--- Backend/src/controllers/billing.controller.js
+++ Backend/src/controllers/billing.controller.js
@@ -3,60 +3,61 @@ const {stripe}=require("../services/stripe.services")
 
 
 async function billing(req,res) {
-    try {
-        const {planType}=req.body
-if(!["free","student","pro"].includes(planType)){
- return res.status(400).json({
-  message:"invalid plan"
- })
-}
-        
-        const userId=req.user._id
-        const plan=plans[planType]
-        if(!plan|| plan.price==0)
-        {
-            return res.status(400).json({
-                message:"free plan not a paid plan "
-            })
-        }  
-      const session = await stripe.checkout.sessions.create({
-        mode:"payment",
-        payment_method_types:["card"],
-        line_items:[
-            {
-                price_data:{
-                    currency:"INR",
-                    product_data:{
-                        name:`SiteGenie.ai ${planType.toUpperCase()} plan`
-                    },
-                    unit_amount:plan.price*100
-                },
-                quantity:1
-            }
-        ],
-        metadata:{
-            userId:userId.toString(),
-            credits:plan.credits,
-            plan:plan.plan
-        },
-        success_url:`${process.env.FRONTEND_URL}/`,
-        cancel_url:`${process.env.FRONTEND_URL}/pricing`
+ try {
 
+  if(!req.user){
+   return res.status(401).json({message:"unauthorized"})
+  }
 
-      })
-      return res.status(200).json({
-        sessionUrl:session.url 
-      })
+  const {planType}=req.body
+  const userId=req.user._id
 
-    } catch (error) {
-       console.log(error);
-       
-       return res.status(500).json({
-        message:"internal server error"
-       })
+  const allowedPlans=["student","pro"]
+
+  if(!allowedPlans.includes(planType)){
+   return res.status(400).json({
+    message:"invalid plan"
+   })
+  }
+
+  const plan=plans[planType]
+
+  const session = await stripe.checkout.sessions.create({
+   mode:"payment",
+   payment_method_types:["card"],
+   line_items:[
+    {
+     price_data:{
+      currency:"INR",
+      product_data:{
+       name:`SiteGenie.ai ${planType.toUpperCase()} Plan`
+      },
+      unit_amount:plan.price*100
+     },
+     quantity:1
     }
-}
+   ],
+   metadata:{
+    userId:userId.toString(),
+    credits:plan.credits,
+    plan:plan.plan
+   },
 
+   success_url:`${process.env.FRONTEND_URL}/dashboard`,
+   cancel_url:`${process.env.FRONTEND_URL}/pricing`
+  })
+
+  return res.status(200).json({
+   sessionUrl:session.url
+  })
+
+ } catch(error){
+  console.log(error)
+  return res.status(500).json({
+   message:"internal server error"
+  })
+ }
+}
 
 
 module.exports={
